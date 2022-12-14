95bb963734a40b62319bf6184c3f3ba891ad0ab0
gnipka <gnipka29@gmail.com>
2022-12-14 20:46:23 +0300

    add library for mvvm

--- Notebook/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Notebook/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Notebook {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Notebook;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Notebook;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
