e0755ed141125c7af51eda6b2ea04b8c46940a49
Anton <anton.gusevv.01@gmail.com>
2022-12-16 19:17:01 +0300

    feat: реализовал методы доступа к таблице user

--- Notebook/Data/Abstract/IRepository.cs
+++ Notebook/Data/Abstract/IRepository.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Notebook.Models;
 
 namespace Notebook.Data.Abstract
 {
     public interface IRepository<T>
     {
+        IReadOnlyCollection<T> GetAll();
 
+        Task<T> GetByIdAsync(int id);
+
+        Task<bool> VerifyUserAsync(string username, string password);
+
+        Task<bool> SaveAsync(T obj);
+
+        Task DeleteAsync(int id);
     }
 }
--- Notebook/Data/Implemetion/EFUserRepository.cs
+++ Notebook/Data/Implemetion/EFUserRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Notebook.Data.Abstract;
 using Notebook.Domain;
 using Notebook.Models;
@@ -16,6 +17,60 @@ namespace Notebook.Data.Implemetion
         public EFUserRepository(ApplicationContext context)
         {
             _context = context;
+        }
+
+        public IReadOnlyCollection<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FirstAsync(m => m.Id == id);
+        }
+
+        public async Task<bool> VerifyUserAsync(string username, string password)
+        {
+            //TODO: пока без шифрования
+            var value = await _context.Users.FirstOrDefaultAsync(x => (x.Username == username && x.Password == password));
+            return value != null;
+        }
+
+        public async Task<bool> SaveAsync(User user)
+        {
+            try
+            {
+                if (user.Id == 0)
+                    await _context.Users.AddAsync(user);
+                else
+                {
+                    var dbEntry = await _context.Users.FirstOrDefaultAsync(u => u.Id == user.Id);
+                    if (dbEntry != null)
+                    {
+                        dbEntry.Username = user.Username;
+                        dbEntry.Password = user.Password;
+                        dbEntry.DateRegister = user.DateRegister;
+                        dbEntry.GraphKeyPoints = user.GraphKeyPoints;
+                        dbEntry.Notes = user.Notes;
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var value = await _context.Users.FindAsync(id);
+            if (value != null)
+                _context.Users.Remove(value);
+            await _context.SaveChangesAsync();
         }
     }
 }
